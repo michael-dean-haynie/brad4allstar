f5d225237240080513441ef54a341bd14540b68a
michael-dean-haynie <michael.dean.haynie@gmail.com>
2016-10-10 20:13:24 -0700

    Added click ribbons to /home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,5 +2,20 @@
 
 
 @section('master-content')
-
+	<div class="jumbotron-container">
+		<div class="jumbotron-image">
+			<div class="click-ribbon-container">
+				<div class="click-ribbon why-ribbon">
+					<div class="click-ribbon-body"></div><!--
+					This Comment removes the space inbetween inline-block elements
+					--><div class="click-ribbon-tip"></div>
+				</div>
+				<div class="click-ribbon how-ribbon">
+					<div class="click-ribbon-body"></div><!--
+					This Comment removes the space inbetween inline-block elements
+					--><div class="click-ribbon-tip"></div>
+				</div>
+			</div>
+		</div>
+	</div>
 @endsection
